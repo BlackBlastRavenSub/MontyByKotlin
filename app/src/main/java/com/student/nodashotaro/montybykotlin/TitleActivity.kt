92e9dfe7ca399b6aaf0f97e800723cab55ce6680
KOGUMA <smallbear0608@gmail.com>
2018-06-22 17:04:47 +0900

    //

--- app/src/main/java/com/student/nodashotaro/montybykotlin/TitleActivity.kt
+++ app/src/main/java/com/student/nodashotaro/montybykotlin/TitleActivity.kt
@@ -3,8 +3,10 @@ package com.student.nodashotaro.montybykotlin
 import android.app.AlertDialog
 import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.Toast
@@ -13,11 +15,40 @@ import org.jetbrains.anko.db.createTable
 import org.jetbrains.anko.db.insert
 import org.jetbrains.anko.db.select
 import org.jetbrains.anko.sdk25.coroutines.onClick
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+
+
+
 
 class TitleActivity : AppCompatActivity() {
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
+    private var mStorageRef: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        mStorageRef = FirebaseStorage.getInstance().getReference()
+        val key = "color"
+        val value = "red"
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference(key)
+        ref.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.value
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("onCancelled", "error:", error.toException())
+            }
+        })
+
 
         verticalLayout {
             padding = dip(30)
@@ -28,29 +59,28 @@ class TitleActivity : AppCompatActivity() {
             editText {
                 hint = "Password"
                 textSize = 24f
+                textColor= Color.RED
             }
             button("Input") {
                 textSize = 26f
                 onClick {
-                    database.use {
-                        insert("HighScore",
-                                "score" to 20
-                        )
-                    }
+                    ref.setValue(value)
                 }
+            }.lparams(width= matchParent){
+                horizontalMargin=dip(5)
+                topMargin=dip(10)
+
             }
             button("Putout") {
                 textSize = 26f
                 onClick {
-                    db.select//dbが使えない？
-                    val test:String=database.use {
-                         select("HighScore", "score")
-                        //Stringに出来ないからデータを見時として画面に表示できない・・
-                    }
-                    ctx.toast(test)
+                    this@button.text = value.toString()
                 }
+            }.lparams(width= matchParent){
+                horizontalMargin=dip(5)
+                topMargin=dip(10)
             }
-            textView("Hello")
         }
     }
+
 }
